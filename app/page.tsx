0820469f039a10a4df71d4a1696a9ef1b8d5e451
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-06-24 10:18:13 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,6 @@ import Link from "next/link"
 import { siteConfig } from "@/config/site"
 import { buttonVariants } from "@/components/ui/button"
 
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
-
 export default function IndexPage() {
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
