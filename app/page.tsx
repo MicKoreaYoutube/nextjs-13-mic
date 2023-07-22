e15ac6a070fb27cbb4bdc8a7cb06ea3a5710821f
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-07-22 14:50:40 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,9 @@ import { buttonVariants } from "@/components/ui/button"
 
 export default function IndexPage() {
   return (
-    <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <Image src="/main_img.png" alt="설명"/>
+    <div>
+      <Image src="/main_img.png" alt="설명" height="1000" width="1000"/>
+      <section className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-2">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           GROUP FOUR 치킨집
@@ -35,5 +36,6 @@ export default function IndexPage() {
         </Link>
       </div>
     </section>
+    </div>
   )
 }
